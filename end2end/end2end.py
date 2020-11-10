10dce0dbc4f581ed150363541f5f1a93076802cc
Lindsay Kempen <code@linths.com>
2020-11-11 00:06:39 +0100

    Use representation learning (pre-training) and finetuning for sequence residual learning

--- end2end/end2end.py
+++ end2end/end2end.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from timeit import default_timer as timer
 import subprocess
+from enum import Enum
 
 from util import *
 from util2 import *
@@ -19,20 +20,27 @@ np.set_printoptions(precision=3)
 
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
-DO_TRAIN = True
+class RunMode(Enum):
+  TRAIN_BASIC = auto()
+  TEST_BASIC = auto()
+  WITH_PRE_TRAIN = auto()
+
+RUN_MODE = RunMode.WITH_PRE_TRAIN
 SAVE_TF = False
 
 WEIGHTS_FILE = 'weights/weights'
+PRE_WEIGHTS_FILE = 'pre-weights/pre-weights'
 TRAIN_DIR = '../data/data_split/train'
 TEST_DIR = '../data/data_split/test'
 TF_DIR = '../data/tf'
 TRAIN_TF_DIR = f'{TF_DIR}/train'
 TEST_TF_DIR = f'{TF_DIR}/test'
 
-EPOCHS = 100
+EPOCHS = 30
 CONTEXT = 7
 WINDOW = 2*CONTEXT+1
 BATCH_SIZE = 256
+N_CLASSES = 256
 
 # Loading data
 def song_gen(song_path):
@@ -106,56 +114,41 @@ end = timer()
 print(f'[Test] Loaded in {end-start} sec.')
 print(len(test_ds_loaded))
 
-pos_train_ds_loaded = train_ds_loaded.filter(lambda feats, label: label != 0)
-neg_train_ds_loaded = train_ds_loaded.filter(lambda feats, label: label == 0)
+def undersample():
+  pos_train_ds_loaded = train_ds_loaded.filter(lambda feats, label: label != 0)
+  neg_train_ds_loaded = train_ds_loaded.filter(lambda feats, label: label == 0)
 
-npos_train = ds_len(pos_train_ds_loaded)
-nneg_train = ds_len(neg_train_ds_loaded)
-rate_train = npos_train / (npos_train + nneg_train)
-print(f'[Train] {rate_train * 100}% positive rate. {npos_train} vs {nneg_train}')
-train_neg_shift = int(1/rate_train * 256)
-print(train_neg_shift)
-neg_train_ds_loaded = neg_train_ds_loaded.window(size=1, shift=train_neg_shift).flat_map(lambda x,y: tf.data.Dataset.zip((x.batch(1), y.batch(1)))).unbatch()
-print(f'[Train] Picked only {ds_len(neg_train_ds_loaded)} NO_STEPs, using shift={train_neg_shift}')
+  npos_train = ds_len(pos_train_ds_loaded)
+  nneg_train = ds_len(neg_train_ds_loaded)
+  rate_train = npos_train / (npos_train + nneg_train)
+  print(f'[Train] {rate_train * 100}% positive rate. {npos_train} vs {nneg_train}')
+  train_neg_shift = int(1/rate_train * N_CLASSES)
+  print(train_neg_shift)
+  neg_train_ds_loaded = neg_train_ds_loaded.window(size=1, shift=train_neg_shift).flat_map(lambda x,y: tf.data.Dataset.zip((x.batch(1), y.batch(1)))).unbatch()
+  print(f'[Train] Picked only {ds_len(neg_train_ds_loaded)} NO_STEPs, using shift={train_neg_shift}')
 
-balanced_train = tf.data.experimental.sample_from_datasets([pos_train_ds_loaded, neg_train_ds_loaded], weights=[1/256, 255/256])
-balanced_train = balanced_train.batch(BATCH_SIZE)
+  balanced_train = tf.data.experimental.sample_from_datasets([pos_train_ds_loaded, neg_train_ds_loaded], weights=[1/N_CLASSES, (N_CLASSES-1)/N_CLASSES])
+  balanced_train = balanced_train.batch(BATCH_SIZE)
+  return balanced_train
+
+balanced_train = undersample()
 test_ds_loaded = test_ds_loaded.batch(BATCH_SIZE)
+train_ds_loaded = train_ds_loaded.batch(BATCH_SIZE)
 
-# Model architecture
-class MyModel(Model):
-  def __init__(self):
-    super(MyModel, self).__init__()
-    self.conv1 = Conv2D(filters=10, kernel_size=(7,3), strides=(1,1), activation='relu')
-    self.max1 = MaxPool2D(pool_size=(1,3), strides=(1,3))
-    self.conv2 = Conv2D(filters=20, kernel_size=(3,3), strides=(1,1), activation='relu')
-    self.max2 = MaxPool2D(pool_size=(1,3), strides=(1,3))
-    n_audiofeats = 8 * 20
-    self.flatten = lambda x: tf.reshape(x, [x.shape[0], 7, n_audiofeats])
-    self.rnn = RNN(StackedRNNCells([LSTMCell(units=512, dropout=0.5) for _ in range(2)]))
-    self.fc1 = Dense(units=256, activation="relu")
-
-  def call(self, x):
-    x = self.conv1(x)
-    x = self.max1(x)
-    x = self.conv2(x)
-    x = self.max2(x)
-    x = self.flatten(x)
-    x = self.rnn(x)
-    return self.fc1(x)
-
-# Create an instance of the model
-model = MyModel()
-
-# Model compile settings
-# class_weights = [0.03] + 255*[0.97] # maybe everything /256
+# === MODEL COMPILE SETTINGS ===
+class_weights = [0.015] + 255*[0.985/256] # maybe everything /256
 # ratio_hit = 0.0000000000001
 # class_weights = [ratio_hit] + 255*[1-ratio_hit] # maybe everything /256
 # class_weights = [.5] + 255*[.5 / 255]
 # class_weights = 256*[1]
 
 loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-optimizer = tf.keras.optimizers.Adam()
+pre_lr = 0.001
+cnn_lr = 0.0000001
+res_lr = 0.00001
+pre_optimizer = tf.keras.optimizers.Adam(learning_rate=pre_lr)
+cnn_optimizer = tf.keras.optimizers.Adam(learning_rate=cnn_lr)
+res_optimizer = tf.keras.optimizers.Adam(learning_rate=res_lr)
 train_loss = tf.keras.metrics.Mean(name='train_loss')
 train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
 test_loss = tf.keras.metrics.Mean(name='test_loss')
@@ -189,6 +182,58 @@ def test_step(images, labels):
   test_loss(t_loss)
   test_accuracy(labels, predictions)
   return predictions
+
+@tf.function
+def pre_train_step(images, labels):
+  with tf.GradientTape() as tape:
+    # training=True is only needed if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    predictions = pre_model(images, training=True)
+    # loss = loss_object(labels, predictions)
+    weighted_logits = predictions * class_weights
+    loss = loss_object(labels, weighted_logits)
+  gradients = tape.gradient(loss, pre_model.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, pre_model.trainable_variables))
+
+  train_loss(loss)
+  train_accuracy(labels, predictions)
+  return predictions
+
+@tf.function
+def pre_test_step(images, labels):
+  # training=False is only needed if there are layers with different
+  # behavior during training versus inference (e.g. Dropout).
+  predictions = pre_model(images, training=False)
+  t_loss = loss_object(labels, predictions)
+  # weighted_logits = predictions * class_weights
+  # t_loss = loss_object(labels, weighted_logits)
+  test_loss(t_loss)
+  test_accuracy(labels, predictions)
+  return predictions
+
+@tf.function
+def train_after_pre_step(images, labels):
+  with tf.GradientTape() as tape:
+    # training=True is only needed if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    predictions = model(images, training=True)
+    # loss = loss_object(labels, predictions)
+    weighted_logits = predictions * class_weights
+    loss = loss_object(labels, weighted_logits)
+  vars = model.trainable_variables
+  cnn_vars = vars[-4:]
+  res_vars = vars[:-4]
+  gradients = tape.gradient(loss, res_vars + cnn_vars)
+  # res_optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+  cnn_grads = gradients[-4:] # cnn layers gradients
+  res_grads = gradients[:-4] # other gradients
+  cnn_optimizer.apply_gradients(zip(cnn_grads, cnn_vars))
+  res_optimizer.apply_gradients(zip(res_grads, res_vars))
+
+  train_loss(loss)
+  train_accuracy(labels, predictions)
+  return predictions
+
 
 def train(train_ds, test_ds):
   print(f"Train and test for {EPOCHS} epochs")
@@ -255,10 +300,94 @@ def test(test_ds, show_confmat=False):
     plt.show() # TODO plt.savefig
   return y_pred
 
-# Run the NN
-if DO_TRAIN:
+def pre_train(train_ds, test_ds, epochs=EPOCHS):
+  print(f"PRE-train and test for {epochs} epochs")
+
+  for epoch in range(epochs):
+    start = timer()
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    test_loss.reset_states()
+    test_accuracy.reset_states()
+
+    for feats, labels in train_ds:
+      pre_train_step(np.asarray(feats), np.asarray(labels))
+    for feats, labels in test_ds:
+      last_layer = pre_test_step(feats, labels)
+      preds = np.argmax(last_layer, axis=1)
+      print(f'0s @ {[i for i, step in enumerate(preds) if step == 0]}')
+      print(f'xs @ {[i for i, step in enumerate(preds) if step != 0]}')
+      print([num2step(pred) for pred in preds])
+
+    end = timer()
+
+    print(
+      f'Epoch {epoch + 1}, '
+      f'Loss: {train_loss.result()}, '
+      f'Accuracy: {train_accuracy.result() * 100}, '
+      f'Test Loss: {test_loss.result()}, '
+      f'Test Accuracy: {test_accuracy.result() * 100}, '
+      f'Time: {end - start}s'
+    )
+    pre_model.save_weights(PRE_WEIGHTS_FILE+f'_epoch_{epoch}')#, save_format='h5')
+  pre_model.save_weights(PRE_WEIGHTS_FILE)#, save_format='h5')
+
+def train_after_pre(train_ds, test_ds, epochs=EPOCHS):
+  print(f"Train and test for {epochs} epochs (after pre)")
+  # model.load_weights(PRE_WEIGHTS_FILE, by_name=True)
+
+  for epoch in range(epochs):
+    start = timer()
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    test_loss.reset_states()
+    test_accuracy.reset_states()
+
+    for feats, labels in train_ds:
+      train_after_pre_step(np.asarray(feats), np.asarray(labels))
+    for feats, labels in test_ds:
+      last_layer = test_step(feats, labels)
+      preds = np.argmax(last_layer, axis=1)
+      print(f'0s @ {[i for i, step in enumerate(preds) if step == 0]}')
+      print(f'xs @ {[i for i, step in enumerate(preds) if step != 0]}')
+      print([num2step(pred) for pred in preds])
+
+    end = timer()
+
+    print(
+      f'Epoch {epoch + 1}, '
+      f'Loss: {train_loss.result()}, '
+      f'Accuracy: {train_accuracy.result() * 100}, '
+      f'Test Loss: {test_loss.result()}, '
+      f'Test Accuracy: {test_accuracy.result() * 100}, '
+      f'Time: {end - start}s'
+    )
+    model.save_weights(WEIGHTS_FILE+f'_epoch_{epoch}')
+  model.save_weights(WEIGHTS_FILE)
+
+
+# === RUNNING ===
+def load_pretrained_weights(model):
+  pre_model.load_weights(PRE_WEIGHTS_FILE)
+  model.conv1 = pre_model.conv1
+  model.max1 = pre_model.max1
+  model.conv2 = pre_model.conv2
+  model.max2 = pre_model.max2
+  assert model.variables != []
+
+if RUN_MODE == RunMode.WITH_PRE_TRAIN:
+  pre_model = PreTrainModel()
+  pre_train(balanced_train, test_ds_loaded, epochs=5)
+  model = ChoreographModel()
+  load_pretrained_weights(model)
+  train_after_pre(train_ds_loaded, test_ds_loaded, epochs=5)
+elif RUN_MODE == RunMode.TRAIN_BASIC:
+  model = ChoreographModel()
   train(balanced_train, test_ds_loaded)
   # train(train_ds_loaded, test_ds_loaded)
-else:
+elif RUN_MODE == RunMode.TEST_BASIC:
+  model = ChoreographModel()
   predicted_steps = test(test_ds_loaded)
   print([i for i, step in enumerate(predicted_steps) if step != 0])
+else:
+  print('Haven\'t selected a run mode.')
---
+++ end2end/models.py
@@ -0,0 +1,42 @@
+# Model architecture
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, Conv3D, MaxPool2D, MaxPool3D, LSTM, LSTMCell, StackedRNNCells, RNN
+from tensorflow.keras import Model
+
+class ChoreographModel(Model):
+  def __init__(self):
+    super(ChoreographModel, self).__init__()
+    self.conv1 = Conv2D(filters=10, kernel_size=(7,3), strides=(1,1), activation='relu', name='conv1')
+    self.max1 = MaxPool2D(pool_size=(1,3), strides=(1,3), name='max1')
+    self.conv2 = Conv2D(filters=20, kernel_size=(3,3), strides=(1,1), activation='relu', name='conv2')
+    self.max2 = MaxPool2D(pool_size=(1,3), strides=(1,3), name='max2')
+    n_audiofeats = 8 * 20
+    self.flatten = lambda x: tf.reshape(x, [x.shape[0], 7, n_audiofeats])
+    self.rnn = RNN(StackedRNNCells([LSTMCell(units=512, dropout=0.5) for _ in range(2)]), name='rnn')
+    self.fc = Dense(units=N_CLASSES, activation="relu", name='final')
+
+  def call(self, x):
+    x = self.conv1(x)
+    x = self.max1(x)
+    x = self.conv2(x)
+    x = self.max2(x)
+    x = self.flatten(x)
+    x = self.rnn(x)
+    return self.fc(x)
+
+class PreTrainModel(Model):
+  def __init__(self):
+    super(PreTrainModel, self).__init__()
+    self.conv1 = Conv2D(filters=10, kernel_size=(7,3), strides=(1,1), activation='relu', name='conv1')
+    self.max1 = MaxPool2D(pool_size=(1,3), strides=(1,3), name='max1')
+    self.conv2 = Conv2D(filters=20, kernel_size=(3,3), strides=(1,1), activation='relu', name='conv2')
+    self.max2 = MaxPool2D(pool_size=(1,3), strides=(1,3), name='max2')
+    self.pre_flatten = Flatten(name='pre_flatten')
+    self.pre_fc = Dense(units=N_CLASSES, activation="relu", name='pre-final')
+
+  def call(self, x):
+    x = self.conv1(x)
+    x = self.max1(x)
+    x = self.conv2(x)
+    x = self.max2(x)
+    x = self.pre_flatten(x)
+    return self.pre_fc(x)
